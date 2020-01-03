0358daa4ebe3db4853f4cf961b84643431d91974
Peter James <peter.james@hcahealthcare.com>
2020-01-02 19:54:25 -0600

    Day 1 solution

---
+++ day1/solution.py
@@ -0,0 +1,39 @@
+import functools
+
+fuels = []
+
+with open('input.txt') as file:
+    masses = file.readlines()
+
+
+# fuel calculation function
+def calculate_fuel(mass):
+    return (int(mass) // 3) - 2
+
+
+# part 1 - iterate over inputs and calculate mass, add all together
+def part1():
+    for mass in masses:
+        fuels.append(calculate_fuel(mass))
+
+    return functools.reduce(lambda a, b: a + b, fuels)
+
+
+# iterate for a single module
+def part2(mass):
+    part2_sum = 0
+    res = calculate_fuel(mass)
+    while res > 0:
+        part2_sum += res
+        res = calculate_fuel(res)
+    return int(part2_sum)
+
+
+fuel_sum_2 = 0
+for mass in masses:
+    fuel_sum = part2(mass)
+    fuel_sum_2 += fuel_sum
+
+
+print('part1: %s' % part1())
+print('part2: %s' % fuel_sum_2)
